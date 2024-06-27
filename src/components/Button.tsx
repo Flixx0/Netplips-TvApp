f305028e4e6f8305d82cbfb5ea3502d685ccca1b
Joel <joelorgeret@gmail.com>
2024-06-27 17:14:27 +0200

    focus

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import { Text } from '@components/Text';
+import { useTheme } from '@contexts/ThemeContext';
 import { MainTypos } from '@contexts/themeTypes';
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { Pressable } from 'react-native';
 
 type Props = {
@@ -11,8 +12,30 @@ type Props = {
 };
 
 export const Button: FC<Props> = ({ text, textVariant, onPress }) => {
+  const theme = useTheme();
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = useCallback(() => {
+    setFocused(true);
+  }, []);
+
+  const handleBlur = useCallback(() => {
+    setFocused(false);
+  }, []);
+
   return (
-    <Pressable onPress={onPress}>
+    <Pressable
+      onPress={onPress}
+      isTVSelectable
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      style={{
+        borderWidth: 2,
+        borderRadius: 4,
+        paddingHorizontal: 4,
+        borderColor: focused ? theme.colors.white10 : theme.colors.dark,
+      }}
+    >
       <Text variant={textVariant}>{text}</Text>
     </Pressable>
   );
